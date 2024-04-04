4b424e1c9f80adc3d5285445a51ce410bdcf1d0a
Gathuku Samwel <gathsam3446@gmail.com>
2024-04-04 11:51:10 +0300

    FUNCTIONS CONTINUED

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+
+function saturdayFun (activity = "roller-skate") 
+{
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork (job = "go to the office")
+{
+    return `This Monday, I will ${job}.`
+}
+
+function wrapAdjective (wrapWord = '*')
+{
+    return function(word = 'a dedicated programmer'){
+      return `You are ${wrapWord}${word}${wrapWord}`  
+    }
+}
+
+    function wrapAdjective (wrapWord = '||')
+{
+    return function(word = 'a dedicated programmer'){
+      return `You are ${wrapWord}${word}${wrapWord}!`  
+    }
+
+}
